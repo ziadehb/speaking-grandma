6765aacbad6076cf610c174f3ea03f04030ed2f5
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-10 13:24:30 -0500

    Add files via upload

---
+++ grandma.rb
@@ -0,0 +1,13 @@
+def speak_to_grandma(phrase)
+    if phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == "Hi Nana, how are you?"
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "Hi!"
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "WHAT DID YOU EAT TODAY?"
+        return "NO, NOT SINCE 1938!"
+    elsif phrase == "WHAT?"
+        return "NO, NOT SINCE 1938!"
+    end
+end
